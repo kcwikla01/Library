e2841e734adcd784fc7b9a0205f11be5eeec9f4b
kcwikla01 <kamilcwikla01@gmail.com>
2024-02-27 11:22:34 +0100

    collections processing with streams

--- src/library/IO/ConsolePrinter.java
+++ src/library/IO/ConsolePrinter.java
@@ -10,40 +10,35 @@ import java.util.Collection;
 public class ConsolePrinter {
 
     public void printMagazines(Collection<Publication> publications){
-        int countMagazines=0;
-        for (Publication publication : publications)
-        {
-            if (publication instanceof Magazine)
-            {
-                printLine(publication.toString());
-                countMagazines++;
-            }
-        }
-        if (countMagazines==0){
+        long count= publications.stream()
+                .filter(p ->p instanceof Magazine)
+                .map(Publication::toString)
+                .peek(s -> printLine(s))
+                .count();
+        if (count==0){
            printLine("Brak magazynow w bilbiotece");
         }
     }
 
     public void printBooks(Collection<Publication> publications){
-        int countBooks=0;
-        for (Publication publication : publications)
-        {
-            if (publication instanceof Book)
-            {
-                printLine(publication.toString());
-                countBooks++;
-            }
-        }
-        if (countBooks==0){
+
+
+
+       long count= publications.stream()
+               .filter(p ->p instanceof Book)
+               .map(Publication::toString)
+               .peek(s -> printLine(s))
+               .count();
+        if (count==0){
             printLine("Brak ksiazek w bilbiotece");
         }
     }
 
     public void printUsers(Collection<LibraryUser> users)
     {
-        for (LibraryUser user : users) {
-            printLine(user.toString());
-        }
+        users.stream()
+                .map(LibraryUser::toString)
+                .forEach(this::printLine);
     }
     public void printLine(String text){
         System.out.println(text);
